20bf482da338d3f2e76db3b1e1a5e4ae45cbb03d
Karolina Jurczak <inka_c@tlen.pl>
2020-07-27 13:54:21 +0200

    Change log data - fifth step

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cors = require('cors')
 const path = require('path');
 const socket = require('socket.io');
 const mongoose = require('mongoose');
-const aws = require('aws-sdk');
 // import routes
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -49,12 +48,7 @@ app.use((req, res) => {
 // const dbURI = (process.env.NODE_ENV === 'production' ? 'mongodb+srv://$${process.env.login}:${process.env.password}@cluster0.iegdp.gcp.mongodb.net/NewWaveDB?retryWrites=true&w=majority' : 'mongodb://localhost:27017/NewWaveDB');
 // mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let s3 = new aws.S3({
-  accessLogin: process.env.S3_login,
-  accessPass: process.env.S3_password
-});
-
-mongoose.connect(`mongodb+srv://${s3.accessLogin}:${s3.accessPass}@cluster0.iegdp.gcp.mongodb.net/NewWaveDB?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(`mongodb+srv://${process.env.NW_login}:${process.env.NW_password}@cluster0.iegdp.gcp.mongodb.net/NewWaveDB?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.once('open', () => {
